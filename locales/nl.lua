934480b7b6d42055d3d353e8339ce8b37f5297a9
MaDHouSe79 <madhouse1979@gmail.com>
2023-10-16 06:49:25 +0200

    Update nl.lua

--- locales/nl.lua
+++ locales/nl.lua
@@ -1,7 +1,10 @@
+--[[ ===================================================== ]] --
+--[[             MH Car Lift Script by MaDHouSe            ]] --
+--[[ ===================================================== ]] --
 local Translations = {
     notify = {
         ['can_not_use_lift_insice_vehicle'] = "Je kunt de lift niet gebruiken als je in de auto zit..",
-        ['no_access'] = "Je hebt geen toegang om deze lift te gebruiken",
+        ['no_access'] = "Je hebt geen toegang om deze lift te gebruiken"
     },
     menu = {
         ['menu_title'] = "Auto Lift",
@@ -9,7 +12,10 @@ local Translations = {
         ['elevator_down'] = "Lift naar beneden",
         ['elevator_stop'] = "Stop de Lift",
         ['press_to_open'] = "[%{key}] - Lift Menu ID(%{id})",
-        ['menu_close'] = "Sluit",
-    },
+        ['menu_close'] = "Sluit"
+    }
 }
-Lang = Lang or Locale:new({phrases = Translations, warnOnMissing = true})
+Lang = Lang or Locale:new({
+    phrases = Translations,
+    warnOnMissing = true
+})
