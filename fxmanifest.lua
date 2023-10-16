46362bc0db399b87187357799a73214aab0196f0
MaDHouSe79 <madhouse1979@gmail.com>
2023-10-16 06:50:34 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,30 +1,17 @@
+--[[ ===================================================== ]] --
+--[[             MH Car Lift Script by MaDHouSe            ]] --
+--[[ ===================================================== ]] --
 fx_version 'cerulean'
 game 'gta5'
 
-shared_scripts {
-    '@qb-core/shared/locale.lua',
-    'locales/en.lua', -- change to your language (en/nl) for now
-    'config.lua',
-}
+shared_scripts {'@qb-core/shared/locale.lua', 'locales/en.lua', -- change to your language (en/nl) for now
+'config.lua'}
 
-client_scripts {
-    '@PolyZone/client.lua',
-    '@PolyZone/BoxZone.lua',
-    '@PolyZone/EntityZone.lua',
-    '@PolyZone/CircleZone.lua',
-    '@PolyZone/ComboZone.lua',
-	'client/main.lua',
-}
+client_scripts {'@PolyZone/client.lua', '@PolyZone/BoxZone.lua', '@PolyZone/EntityZone.lua', '@PolyZone/CircleZone.lua',
+                '@PolyZone/ComboZone.lua', 'client/main.lua'}
 
-server_scripts {
-	'@oxmysql/lib/MySQL.lua',
-    'server/main.lua',
-	'server/update.lua',
-}
+server_scripts {'@oxmysql/lib/MySQL.lua', 'server/main.lua', 'server/update.lua'}
 
-dependencies {
-    'oxmysql',
-    'qb-core',
-}
+dependencies {'oxmysql', 'qb-core'}
 
 lua54 'yes'
