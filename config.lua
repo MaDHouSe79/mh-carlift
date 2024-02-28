984974d832423e5554781e2ced9bdf36b81cc0df
MaDHouSe <madhouse1979@gmail.com>
2024-02-28 09:09:52 +0100

    Update config.lua

--- config.lua
+++ config.lua
@@ -41,8 +41,96 @@ Config.Fontawesome = {
 }
 
 Config.Elevators = {
-    [1] = {
-        id = 1,
+    -- scraptard pos 1 (in de hal) (left)
+    [1] = { -- this and the id, must be the same number.
+        id = 1, -- this needs the id from above. 
+        min = 47.37, -- lower position of the lift
+        max = 49.45, -- higher position of the lift (z from the main coords)
+        beforemax = 45.53, -- detector for speed_up_slow befor the final point
+        beforemin = 48.00, -- detector for speed_down_slow befor the final point
+        entity = nil, -- dont change this
+        needPoles = true, -- if dont have poles set this true
+        openMenu = true, -- if you want to have a menu on the lift, false if you have other plans with it. (triggering by other scripts)
+        workarea = {
+            length = 7.0,
+            wide = 7.0
+        }, -- length and wide of the box zone
+        job = {"mechanic"}, -- job access
+        coords = {
+            ["x"] = 2345.31,
+            ["y"] = 3141.512,
+            ["z"] = 47.37874,
+            ["h"] = -189.5
+        } -- start base coords of this lift platform.
+    },
+    -- scraptard pos 2 (in de hal) (right)
+    [2] = {
+        id = 2,
+        min = 47.37,
+        max = 49.45,
+        beforemax = 45.53,
+        beforemin = 48.00,
+        entity = nil,
+        needPoles = true,
+        openMenu = true,
+        workarea = {
+            length = 7.0,
+            wide = 7.0
+        },
+        job = {"mechanic"},
+        coords = {
+            ["x"] = 2358.866,
+            ["y"] = 3139.057,
+            ["z"] = 47.37369,
+            ["h"] = -190.5
+        }
+    },
+    -- scraptard pos 3 (outside) (left)
+    [3] = {
+        id = 3,
+        min = 47.31,
+        max = 49.27,
+        beforemax = 45.53,
+        beforemin = 45.95,
+        entity = nil,
+        needPoles = true,
+        openMenu = true,
+        workarea = {
+            length = 7.0,
+            wide = 7.0
+        },
+        job = {"mechanic"},
+        coords = {
+            ["x"] = 2333.153,
+            ["y"] = 3042.031,
+            ["z"] = 47.31144,
+            ["h"] = 1.0
+        }
+    },
+    -- scraptard pos 4 (outside) (right)
+    [4] = {
+        id = 4,
+        min = 47.31,
+        max = 49.27,
+        beforemax = 45.53,
+        beforemin = 45.95,
+        entity = nil,
+        needPoles = true,
+        openMenu = true,
+        workarea = {
+            length = 7.0,
+            wide = 7.0
+        },
+        job = {"mechanic"},
+        coords = {
+            ["x"] = 2339.808,
+            ["y"] = 3042.183,
+            ["z"] = 47.3141,
+            ["h"] = -1.0
+        }
+    },
+    [5] = { -- blockenpark garage
+        id = 5,
         min = 29.8,
         max = 32.0,
         beforemax = 31.5,
@@ -50,7 +138,7 @@ Config.Elevators = {
         elevator = nil, -- the entity 
         needPoles = true,
         openMenu = true,
-		job = {"all"},
+        job = nil, -- nil or {"unemployed", "machenic", "tuner"}
         workarea = {
             length = 7.0,
             wide = 7.0
