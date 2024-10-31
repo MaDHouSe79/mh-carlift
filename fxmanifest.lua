f264da93fbe609d329c0219faa0ebb9bee9f99c8
MaDHouSe <madhouse1979@gmail.com>
2024-10-31 21:28:30 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,14 +4,13 @@
 fx_version 'cerulean'
 game 'gta5'
 
-shared_scripts {'@qb-core/shared/locale.lua', 'locales/en.lua', -- change to your language (en/nl) for now
-'config.lua'}
+author 'MaDHouSe79'
+description 'MH Car Lift'
+version '1.0.0'
 
-client_scripts {'@PolyZone/client.lua', '@PolyZone/BoxZone.lua', '@PolyZone/EntityZone.lua', '@PolyZone/CircleZone.lua',
-                '@PolyZone/ComboZone.lua', 'client/main.lua'}
-
+shared_scripts {'@qb-core/shared/locale.lua', 'locales/en.lua', 'config.lua'}
+client_scripts {'@PolyZone/client.lua', '@PolyZone/BoxZone.lua', '@PolyZone/EntityZone.lua', '@PolyZone/CircleZone.lua', '@PolyZone/ComboZone.lua', 'client/main.lua'}
 server_scripts {'@oxmysql/lib/MySQL.lua', 'server/main.lua', 'server/update.lua'}
-
 dependencies {'oxmysql', 'qb-core'}
 
 lua54 'yes'
